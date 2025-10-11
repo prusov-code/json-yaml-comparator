6b370a8e6ab6c24c1ff31f911d4a364d905919a0
prusov-code <prusov-official@yandex.ru>
2025-10-11 03:06:10 +0300

    Added new tests for comparator for yml format (TDD)

--- tests/ComparatorTest.php
+++ tests/ComparatorTest.php
@@ -2,14 +2,23 @@
 require_once __DIR__."/../vendor/autoload.php";
 
 use PhpUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Hexlet\Code\Parser;
 
 class ComparatorTest extends TestCase
 {
-    public function testCompare():void {
-        $file1=Parser::parseFile('tests/fixtures/file1.json');
-        $file2=Parser::parseFile('tests/fixtures/file2.json');
+    public static function compareProvider(): array
+    {
+        return [
+            'JSON files comparison: '=>['tests/fixtures/file1.json','tests/fixtures/file2.json','/fixtures/comparation_one_result.txt'],
+            'YAML files comparison: '=>['tests/fixtures/file1.yml','tests/fixtures/file2.yml','/fixtures/comparation_one_result.txt']
+        ];
+    }
+    #[DataProvider('compareProvider')]
+    public function testCompare(string $file1Path,string $file2Path, string $resultFilePath):void {
+        $file1=Parser::parseFile($file1Path);
+        $file2=Parser::parseFile($file2Path);
         $diff=\Hexlet\Code\Comparator::compare($file1, $file2);
-        $this->assertStringEqualsFile(__DIR__.'/fixtures/comparation_one_result.txt',$diff);
+        $this->assertStringEqualsFile(__DIR__.$resultFilePath,$diff);
     }
 }
