69b6aab85010bbc39da302764e370d856cc243d6
prusov-code <prusov-official@yandex.ru>
2025-10-15 03:21:15 +0300

    Fix code-style via code sniffer

--- src/Formatters/PlainFormatter.php
+++ src/Formatters/PlainFormatter.php
@@ -5,41 +5,43 @@ namespace Hexlet\Code\Formatters;
 class PlainFormatter
 {
     private const int SPACES_IN_INDENT = 4;
-    public static function formatDiffPlain(array $diff, int $enclosure=1, string $fullPath=''): string
+    public static function formatDiffPlain(array $diff, int $enclosure = 1, string $fullPath = ''): string
     {
         $formattedDiff = '';
         foreach ($diff as $key => $value) {
-            $currentFullPath = $enclosure===1?$key:"$fullPath.$key";
+            $currentFullPath = $enclosure === 1 ? $key : "$fullPath.$key";
             if (isset($value['value']) && is_array($value['value'])) {
                 if (isset($value['state'])) {
                     $status = $value['state'] == 'added' ? 'was added with value: [complex value]' : 'was removed';
                     $formattedDiff .= "Property '$currentFullPath' $status\n";
+                } else {
+                    $formattedDiff .= self::formatDiffPlain($value['value'], $enclosure + 1, $currentFullPath);
                 }
-                else {
-                    $formattedDiff .=self::formatDiffPlain($value['value'],$enclosure+1, $currentFullPath);
-                }
-            }elseif (!isset($value['state']) && is_array($value)) {
-                $formattedDiff .=self::formatDiffPlain($value,$enclosure+1, $currentFullPath);
+            } elseif (!isset($value['state']) && is_array($value)) {
+                $formattedDiff .= self::formatDiffPlain($value, $enclosure + 1, $currentFullPath);
             } elseif (!isset($value['state'])) {
                 continue;
-            }  elseif ($value['state'] == 'changed') {
-                $prevValue=is_array($value['prevValue'])?'[complex value]':self::unStringifyValue($value['prevValue']);
-                $newValue=is_array($value['newValue'])?'[complex value]':self::unStringifyValue($value['newValue']);
+            } elseif ($value['state'] == 'changed') {
+                $unStringifiedPrevValue = self::unStringifyValue($value['prevValue']);
+                $unStringifiedNewValue = self::unStringifyValue($value['newValue']);
+                $prevValue = is_array($value['prevValue']) ? '[complex value]' : $unStringifiedPrevValue;
+                $newValue = is_array($value['newValue']) ? '[complex value]' : $unStringifiedNewValue;
                 $formattedDiff .= "Property '$currentFullPath' was updated. From $prevValue to $newValue\n";
             } elseif ($value['state'] == 'added') {
-                $formattedDiff .= "Property '$currentFullPath' was added with value: ".self::unStringifyValue($value['value'])."\n";
+                $unStringifiedValue = self::unStringifyValue($value['value']);
+                $formattedDiff .= "Property '$currentFullPath' was added with value: $unStringifiedValue\n";
             } elseif ($value['state'] == 'removed') {
                 $formattedDiff .= "Property '$currentFullPath' was removed\n";
             }
         }
         return $formattedDiff;
     }
-    public static function unStringifyValue(mixed $value):mixed {
-        if($value === 'false' || $value === 'true' || $value === 'null') {
+    public static function unStringifyValue(mixed $value): mixed
+    {
+        if ($value === 'false' || $value === 'true' || $value === 'null') {
             return $value;
-        }
-        else {
-            return var_export($value,true);
+        } else {
+            return var_export($value, true);
         }
     }
 }
