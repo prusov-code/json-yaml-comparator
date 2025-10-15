fa1684b5e305fab8fd3015b73d4c1f766ed09c7d
prusov-code <prusov-official@yandex.ru>
2025-10-15 03:41:11 +0300

    Fix, code-style fixes via code sniffer

--- src/Formatters/JsonFormatter.php
+++ src/Formatters/JsonFormatter.php
@@ -4,7 +4,8 @@ namespace Hexlet\Code\Formatters;
 
 class JsonFormatter
 {
-    public static function formatDiffToJson(array $diff):string {
-        return json_encode($diff,JSON_PRETTY_PRINT);
+    public static function formatDiffToJson(array $diff): string
+    {
+        return json_encode($diff, JSON_PRETTY_PRINT);
     }
 }
