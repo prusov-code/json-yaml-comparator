eed1f98c8fe393570835dcc67d66893de838e11f
prusov-code <prusov-official@yandex.ru>
2025-10-15 04:33:36 +0300

    Fix, exception name fixed

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -9,9 +9,9 @@ class ParserTest extends TestCase
     public function testParse()
     {
         $parser = new \Hexlet\Code\Parser();
-        $this->expectException(UnexpectedValueException::class);
+        $this->expectException(\UnexpectedValueException::class);
         $parser::parseFile('fakeFile.yml');
-        $this->expectException(UnexpectedValueException::class);
+        $this->expectException(\UnexpectedValueException::class);
         $parser::parseFile(__DIR__ . '/../files/file1.txt');
     }
 }
